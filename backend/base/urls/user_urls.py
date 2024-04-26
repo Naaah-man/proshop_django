b53a1ea177144c68eac4960470ec9747571e6c8c
Naman Joshi <namanjoshi2581@gmail.com>
2024-04-26 14:15:34 +0530

    payment Screen

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -5,7 +5,12 @@ urlpatterns = [
   
   path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('register/', views.registerUser, name='register'),
+  
+  path('delete/<str:pk>/', views.deleteUser, name="delete_user"),
+  
   path('profile/', views.getUserProfile, name="user_profile"),
+  path('profile/update/', views.updateUser, name="update_user"),
+  
   path('all/', views.getUsers, name="users_all"),
   
 ]
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,23 +1,19 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
+from base.models import *
+from base.serializers import UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-
-from base.serializers import UserSerializer, UserSerializerWithToken
-
+from rest_framework import status
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
-      # to customize the response 
       data = super().validate(attrs)
       
       serializer = UserSerializerWithToken(self.user).data
@@ -26,14 +22,16 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         data[k] = v
       
       return data
-      
+
 class MyTokenObtainPairView(TokenObtainPairView):
   serializer_class = MyTokenObtainPairSerializer
   
+
+
 @api_view(['POST'])
 def registerUser(request):
+  data = request.data
   try:
-    data = request.data
     user = User.objects.create(
       first_name = data['name'],
       username = data['email'],
@@ -42,16 +40,38 @@ def registerUser(request):
     )
     serializer = UserSerializerWithToken(user, many=False)
     return Response(serializer.data)
-  except:
-    message = {'detail': "User with this email already exist"}
+  except: 
+    message = {'detail': 'User with this email already exists'}
     return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
-
+@api_view(['PUT'])
 @permission_classes([IsAuthenticated])
+def updateUser(request):
+    user = request.user
+    serializer = UserSerializerWithToken(user, many=False)
+    
+    data = request.data
+    user.first_name = data['name']
+    user.username = data['name']
+    user.email = data['email']
+    
+    if(data['password'] != ''):
+      user.password = make_password(data['password'])
+      
+    user.save()
+    
+    return Response(serializer.data)
+
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
+  user = request.user
+  serializer = UserSerializer(user, many=False)
+  return Response(serializer.data)
   
-  # unlike as its default behaviour to grab user credentials it will now fetch token credentials
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserById(request):
   user = request.user
   
   serializer = UserSerializer(user, many=False)
@@ -61,6 +81,14 @@ def getUserProfile(request):
 @permission_classes([IsAdminUser])
 def getUsers(request):
   users = User.objects.all()
-  
   serializer = UserSerializer(users, many=True)
   return Response(serializer.data)
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteUser(request):
+  user = request.user 
+  user.delete()
+  
+  message = {"detail": "Action performed successfully"}
+  return Response(message, status=200)
